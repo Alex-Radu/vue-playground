1f59dc6247cb324b9fe679071cf7d9e791af1c66
Alex Radu <alexandru.radu89@gmail.com>
2019-11-17 20:29:59 +0200

    :construction: Added confetti, construction banner and skeleton nav menu

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <div class="card">
+      <h1>🚧 Under construction 🚧</h1>
+    </div>
+    <AvatarMenu/>
+  </div>
+</template>
+
+<script>
+import AvatarMenu from './components/AvatarMenu.vue';
+
+export default {
+  name: 'app',
+  components: {
+    AvatarMenu,
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  box-sizing: border-box;
+  color: #2c3e50;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  height: 100vh;
+  padding-top: 10rem;
+  position: relative;
+  text-align: center;
+  width: 100vw;
+  z-index: 2;
+}
+
+.card {
+  background: white;
+  box-shadow: 6px 6px 5px -4px rgba(130, 130, 130, .8);
+  display: inline-block;
+  margin: 0 auto;
+  padding: 2em;
+
+  h1 {
+    margin: 0;
+  }
+}
+</style>
---
+++ src/components/AvatarMenu.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="avatar-menu">
+        <div class="menu-header">
+          <img src="../../assets/images/avatar.png" alt>
+          <span>Alex Radu</span>
+        </div>
+        <nav>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'avatar-menu',
+};
+</script>
+
+<style lang="scss">
+  .avatar-menu {
+    box-shadow: 6px 6px 5px -4px rgba(130, 130, 130, .8);
+    position: fixed;
+    bottom: 2rem;
+    right: 2rem;
+
+    .menu-header {
+      background: #F87060;
+      color: white;
+      font-weight: bold;
+      display: flex;
+      flex-direction: row;
+      padding: 1rem;
+
+      img {
+        border-radius: 50%;
+        box-shadow: 6px 6px 5px -4px rgba(130, 130, 130, .8);
+        height: 3rem;
+        width: 3rem;
+
+        &:hover {
+          cursor: pointer;
+          box-shadow: 6px 6px 5px -4px rgb(98, 98, 98);
+        }
+      }
+
+      span {
+        align-self: center;
+        margin-left: 1rem;
+      }
+    }
+
+    nav {
+      background: #D7E7ED;
+
+      ul {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+
+        li {
+          a {
+            color: black;
+            display: block;
+            padding: .5rem;
+            text-decoration: none;
+
+            &:hover {
+              background: #9BC3D3;
+              color: white;
+            }
+          }
+        }
+      }
+    }
+
+  }
+</style>
